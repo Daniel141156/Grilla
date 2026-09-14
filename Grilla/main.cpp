02fb117a1c8c73fc2e72f46391b0c43e687427cd
Daniel141156 <est.daniela.fuentesb@unimilitar.edu.co>
2026-09-13 22:34:27 -0500

    Cambio en la construccion de la grilla y bresenham

--- Grilla/main.cpp
+++ Grilla/main.cpp
@@ -2,20 +2,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
-
+#include <cstddef>
 //codigo fuente del Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"    ourColor = aColor;\n"
 "}\0";
 //codigo fuente del Fragment Shader
 const char* fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor;\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"    FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 struct Vertex//!
@@ -23,9 +27,38 @@ struct Vertex//!
 	GLfloat x;
 	GLfloat y;
 	GLfloat z;
-};
 
-void generarGrilla(int columnas, int filas,std::vector<Vertex>& lineVertices){
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+};
+//pintar lineas con bresenham
+void drawLine(int x0, int y0, int x1, int y1, std::vector<Vertex>& vertices, int columnas) {
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int D = 2 * dy - dx;
+	int y = y0;
+		for (int x = x0; x <= x1; x++)
+		{
+		// Pintar la celda (x,y)
+		int celda = y * columnas + x;
+		int base = celda * 4;
+		for (int i = 0; i < 4; i++)
+		{
+			vertices[base + i].r = 0.941f;
+			vertices[base + i].g = 0.941f;
+			vertices[base + i].b = 0.235f;
+		}
+		// Bresenham
+		if (D > 0) {
+			y = y + 1;
+			D = D - 2 * dx;
+		}
+		D = D + 2 * dy;
+	}
+}
+
+/*void generarGrilla(int columnas, int filas, std::vector<Vertex>& lineVertices) {
 	// Líneas verticales
 	for (int columna = 0; columna <= columnas; columna++)
 	{
@@ -43,7 +76,7 @@ void generarGrilla(int columnas, int filas,std::vector<Vertex>& lineVertices){
 		lineVertices.push_back({ -1.0f, y, 0.0f });
 		lineVertices.push_back({ 1.0f, y, 0.0f });
 	}
-}
+}*/
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -85,47 +118,46 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	int columnas = 80;
-	int filas = 80;
+	int columnas = 40;
+	int filas = 40;
 
 	std::vector<Vertex> vertices;//!
 	std::vector<GLuint> indices;//!
-	std::vector<Vertex> lineVertices;//!
+	//std::vector<Vertex> lineVertices;//!
+	//indices
 	for (int fila = 0; fila < filas; fila++)
 	{
 		for (int columna = 0; columna < columnas; columna++)
 		{
-			float x0 = -1.0f + 2.0f * columna / (float)columnas;
-			float x1 = -1.0f + 2.0f * (columna + 1) / (float)columnas;
+			float separacion = 0.002f;
 
-			float y0 = -1.0f + 2.0f * fila / (float)filas;
-			float y1 = -1.0f + 2.0f * (fila + 1) / (float)filas;
+			float x0 = -1.0f + 2.0f * columna / (float)columnas + separacion;
+			float x1 = -1.0f + 2.0f * (columna + 1) / (float)columnas - separacion;
+
+			float y0 = 1.0f - 2.0f * fila / (float)filas - separacion;
+			float y1 = 1.0f - 2.0f * (fila + 1) / (float)filas + separacion;
 
 			GLuint base = static_cast<GLuint>(vertices.size());
 
-			vertices.push_back({ x0, y1, 0.0f });
-			vertices.push_back({ x1, y1, 0.0f });
-			vertices.push_back({ x1, y0, 0.0f });
-			vertices.push_back({ x0, y0, 0.0f });
+			// 4 vertices del cuadrado
+			vertices.push_back({ x0, y1, 0.0f, 0.212f, 0.278f, 0.463f });
+			vertices.push_back({ x1, y1, 0.0f, 0.212f, 0.278f, 0.463f });
+			vertices.push_back({ x1, y0, 0.0f, 0.212f, 0.278f, 0.463f });
+			vertices.push_back({ x0, y0, 0.0f, 0.212f, 0.278f, 0.463f });
 
-			// borde superior
+			//Primer triángulo
 			indices.push_back(base + 0);
-			indices.push_back(base + 1);
-
-			// borde derecho
 			indices.push_back(base + 1);
 			indices.push_back(base + 2);
 
-			// borde inferior
+			//Segundo triángulo
+			indices.push_back(base + 0);
 			indices.push_back(base + 2);
 			indices.push_back(base + 3);
-
-			// borde izquierdo
-			indices.push_back(base + 3);
-			indices.push_back(base + 0);
-
 		}
 	}
+	int x0 = 7, y0 = 5, x1 = 30, y1 = 25;
+	drawLine(x0, y0, x1, y1, vertices, columnas);
 	//generarGrilla(columnas, filas, lineVertices);
 	/*
 	GLfloat vertices[] = {
@@ -157,7 +189,7 @@ int main() {
 		4, 7, 8,
 		4, 5, 8
 	};*/
-	GLuint VAO, VBO, EBO, lineasVAO, lineasVBO;
+	GLuint VAO, VBO, EBO;
 	//el 1 significa que queremos generar un objeto, & guarda el identificador que genere opengl en la variable, en este caso las variables son VAO, VBO, EBO.
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -175,6 +207,7 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(),GL_STATIC_DRAW);
 	//lo mismo con el EBO, element array buffer viuene del mismo EBO, element buffer object
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(),GL_STATIC_DRAW);
 	
 	//indica como leer los datos del VBO,  0 es el location, coincide con el vertexshadersource
@@ -189,23 +222,20 @@ int main() {
 	//la posición empieza en el byte 0 
 	
 	// en este caso location=0 indica el atributo de posicion, el atributo 0
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(GLfloat),(void*)0);//location que atributo es, size cuantos valores, type, stride cuanto ocupa cada fila, offset donde empieza
+	//posicion
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, x));//location que atributo es, size cuantos valores, type, stride cuanto ocupa cada fila, offset donde empieza
 	glEnableVertexAttribArray(0);//activa el atributo numero 0
+	//color
+	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,sizeof(Vertex),(void*)offsetof(Vertex, r));
+	glEnableVertexAttribArray(1);
 	//deja de seleccionar los VBO y VAO, practica para evitar modificaciones accidentales
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glGenVertexArrays(1, &lineasVAO);
-	glGenBuffers(1, &lineasVBO);
-	glBindVertexArray(lineasVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, lineasVBO);
-	glBufferData(GL_ARRAY_BUFFER,lineVertices.size() * sizeof(Vertex),lineVertices.data(),GL_STATIC_DRAW);
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(GLfloat),(void*)0);
-	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClearColor(0.204f, 0.216f, 0.251f, 1.0f);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -214,7 +244,7 @@ int main() {
 		//para dibujar, gltriangles interpreta indices de 3 en 3 para formar triangulos, 24 cantidad de indices, undigned int pq los indices son gluint, 0 desde la posicion del EBO que se va a leer, 
 		//pero es la orden final
 		//cuadrados
-		glDrawElements(GL_LINES, indices.size(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
 		//glDrawElements(GL_TRIANGLES, /*24*/ indices.size(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
 		//lineas de la grilla
 		//glBindVertexArray(lineasVAO);
@@ -222,8 +252,6 @@ int main() {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	glDeleteVertexArrays(1, &lineasVAO);
-	glDeleteBuffers(1, &lineasVBO);
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
